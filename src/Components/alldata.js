96568b33d913da7a9e7b5052fa0e5cfb015cb418
Sarah Ibarra-Scurr <sarah.elfrank@gmail.com>
2023-01-13 07:55:58 -0600

    making all data a card component

--- src/Components/alldata.js
+++ src/Components/alldata.js
@@ -12,6 +12,27 @@ const AllData=()=>{
  const ctx = React.useContext(UserContext);
  const accounts = ctx.users;
  console.log('ctx.users, alldata',ctx.users);
+
+ const Card=(props)=>{
+  function classes(){
+    const bg  = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
+    const txt = props.txtcolor ? ' text-' + props.txtcolor: ' text-white';
+    return 'card mb-3 ' + bg + txt;
+  }
+
+  return (
+    <div className={classes()} style={{maxWidth: "50rem"}}>
+      <div className="card-header">{props.header}</div>
+      <div className="card-body">
+        {props.title && (<h5 className="card-title">{props.title}</h5>)}
+        {props.text && (<p className="card-text">{props.text}</p>)}
+        {props.body} 
+        {props.status && (<div id='createStatus'>{props.status}</div>)}
+        
+      </div>
+    </div>      
+  );    
+}
  
  const accountName = accounts.map((anObjectMapped, index) =>{
   return (
@@ -37,18 +58,25 @@ const AllData=()=>{
  console.log('Alldata',accounts)
  console.log('addData account', accountName)
   return (
-    <>
-    <div style={{backgroundColor: 'white'}}>
-    <h2 style={{paddingBottom:"10px",paddingTop: "10px",color:"#6c757d"}}>All Users</h2>
+    
+    <Card
+    bgcolor="secondary"
+    header="All Data"
+      body={
+<>
+    <div>
+    
   
   <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 0,backgroundColor: '#6c757d', color:"white" }}>
     <div><h4 style={{color:"white", borderBottom: "solid white"}}>Name </h4>{accountName}</div>
     <div><h4 style={{color:"white", borderBottom: "solid white"}}>Email </h4> {accountEmail}</div>
     <div><h4 style={{color:"white", borderBottom: "solid white"}}>Password </h4> {accountPassword}</div>
   </div><br/>
-  <NavLink to="/createaccount/" style={{color:"#6c757d",  fontStyle: "italic"}}>Add Additional Users</NavLink>
+  <NavLink to="/createaccount/" style={{color:"white",  fontStyle: "italic"}}>Add Additional Users</NavLink>
     </div>
-    </> 
+      </> }
+    />
+  
   ); 
 };
 
